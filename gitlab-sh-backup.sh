f5c0f78ad64c59cbe4b1f67cde172d701349ac07
root <root@satellite.lab.example.net>
2022-07-18 15:08:45 +1000

    Preserved subfolder structure

--- gitlab-sh-backup.sh
+++ gitlab-sh-backup.sh
@@ -9,6 +9,7 @@
 #curl  -sH "Authorization: Bearer $GITLAB_TOKEN" ${GITLAB_SERVER}/api/v4/groups/${GITLAB_GROUP}/projects | jq .[].web_url 	
 #list all projects
 #curl  -sH "Authorization: Bearer $GITLAB_TOKEN" ${GITLAB_SERVER}/api/v4/projects | jq .[].ssh_url_to_repo
+WORK_DIR=`pwd`
 HEADER="Authorization: Bearer ${GITLAB_TOKEN}"
 PROJECT_ATTRIBUTES="per_page=100"
 RESPONSE=$( \
@@ -35,20 +36,24 @@ if [[ "$LAST_PAGE" -gt "1" ]]; then
     ALL_PROJECTS+=$'\n'"$RESPONSE"
   done
 fi
-echo $ALL_PROJECTS | jq | grep -i ${GITLAB_GROUP} | sed -e 's/.*://g' -e 's/"//g' | xargs -n 1 -I {}  git clone https://${GITLAB_USER}:${GITLAB_TOKEN}@${GITLAB_SERVER}/{}
+#echo $ALL_PROJECTS | jq | grep -i ${GITLAB_GROUP} | sed -e 's/.*://g' -e 's/"//g' | xargs -n 1 -I {}  git clone https://${GITLAB_USER}:${GITLAB_TOKEN}@${GITLAB_SERVER}/{}
 
 #Process all repos we pulled down
-for GIT_REPO in $( echo $ALL_PROJECTS | jq | grep -i ${GITLAB_GROUP} | sed -e 's/.*://g' -e 's/"//g' | xargs -n 1 basename -s .git)
+for GIT_REPO in $( echo $ALL_PROJECTS | jq | grep -i ${GITLAB_GROUP} | sed -e 's/.*://g' -e 's/"//g' )
   do
-    #enter each repo
-    cd $GIT_REPO
+    #make subdir and clone
+    mkdir -p `dirname ${GIT_REPO}`
+    cd `dirname ${GIT_REPO}`
+    git clone https://${GITLAB_USER}:${GITLAB_TOKEN}@${GITLAB_SERVER}/${GIT_REPO}
+    cd `basename -s .git ${GIT_REPO}`
     #Pull down all branches
-    for branch in $(git branch --all | grep '^\s*remotes' | egrep --invert-match '(HEAD)'); do
-    echo "Now pulling branch ${branch} for ${GIT_REPO}"
-    git branch --track "${branch##*/}" "$branch"
+    for branch in $(git branch --all | grep '^\s*remotes' | egrep --invert-match '(HEAD)'); 
+    do
+    	echo "Now pulling branch ${branch} for ${GIT_REPO}"
+    	git branch --track "${branch##*/}" "$branch"
     done
     #exit 
-    cd ..
+    cd ${WORK_DIR}
 done
  
 #curl  -H "Authorization: Bearer $GITLAB_TOKEN" ${GITLAB_SERVER}/api/v4/groups/${GITLAB_GROUP}/projects | jq .[].ssh_url_to_repo
